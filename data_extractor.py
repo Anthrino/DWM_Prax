de3a079e212ea5b4d324a312b44809ac3d546133
Jerin John <jerin.j@somaiya.edu>
2017-10-20 21:26:52 +0530

    Data ETL Completed.

--- data_extractor.py
+++ data_extractor.py
@@ -1,3 +1,5 @@
+# Data extraction, transformation and loading (ETL) operations from source files to MongoD collections
+
 import json
 import csv
 import openpyxl as xl
@@ -6,43 +8,72 @@ from pprint import pprint
 
 mdb_client = MongoClient()
 
-db = mdb_client.uefa.clubs
-print("\n> UEFA Clubs collection selected.")
+db = mdb_client.uefa.leagues
 db.remove({})
 
-print("> Inserted PL data into UEFA Clubs Collection : \n")
+# Loading league profiles data from csv file
+with open('LeagueStats.csv') as csv_source:
+	source_data = csv.reader(csv_source)
+	for line in source_data:
+		# print(line)	
+		if line[0] != 'L_Id':
+			db.insert_one({"_id": line[0], "name": line[1], "country": line[1] , "team_count": line[2], 
+				"'Avg_Transfer_spending": line[6]})
 
-# Loading clubs data from json file
-with open('uefa_country_clubs.json') as json_source: 
-	source_data = json.load(json_source)
-	# pprint(source_data['tournaments'])
+print("> Inserted League data into UEFA League Collection\n")
 
-	for x in source_data['tournaments']:
-		# pprint(x['name'])
-		if x['name'] == "English Premier League 2015/16":
-			for y in x['clubs']:
-				db.insert_one(y)
+db = mdb_client.uefa.clubs
+db.remove({})
 
-for doc in db.find():
-	print(doc)
+# Loading club profiles data from excel file
+club_profile_sheet = xl.load_workbook('ClubProfile.xlsx').active
+
+for x in club_profile_sheet:
+	# print(x[0].value+str(x[1].value)+x[2].value+x[3].value)
+	if x[3].value != 'Club_Id':
+		db.insert_one({"_id": x[3].value, "name": x[0].value, "budget": x[1].value , "manager": x[2].value,
+	 		"league_id": x[4].value, "global_ranking": x[5].value})
+
+print("> Inserted Clubs data into UEFA Clubs Collection\n")
+
+db = mdb_client.uefa.agents
+db.remove({})
+
+# Loading agents profiles data from csv file
+with open('AgentProfile.csv') as csv_source:
+	source_data = csv.reader(csv_source)
+	for line in source_data:
+		# print(line)	
+		if line[1] != 'Agent_Id':
+			db.insert_one({"_id": line[1], "name": line[0], "popularity": line[2]})
+
+print("> Inserted Agents data into UEFA Agent Collection\n")
 
 db = mdb_client.uefa.playerprofiles
-print("\n> UEFA Player Profiles collection selected.")
 db.remove({})
 
-print("> Inserted Player data into UEFA Profiles Collection : \n")
 
-# Loading player prfiles data from excel file
+# Loading player profiles data from excel file
 player_profile_sheet = xl.load_workbook('PlayerProfile.xlsx').active
 
 for x in player_profile_sheet:
 	# print(x[0].value+str(x[1].value)+x[2].value+x[3].value)
-	db.insert_one({"name": x[0].value, "age": x[1].value , "position": x[2].value, "valuation": x[3].value})
+	if x[5].value != 'Player_Id':
+		db.insert_one({"_id": x[5].value, "name": x[0].value, "age": x[1].value , "position": x[2].value, 
+			"valuation": x[3].value, "rating": x[4].value, "agent_id": x[6].value, "club_id": x[7].value})
 
-for doc in db.find():
-	print(doc)
+print("> Inserted Player data into UEFA Profiles Collection\n")
 
-# with open('file.csv') as csv_source:
-# 	source_data = csv.reader(csv_source)
-# 	for line in reader:
-# 		print(line)
+# for doc in db.find():
+# 	print(doc)
+
+# print("> Inserted PL data into UEFA Clubs Collection : \n")
+# Loading clubs data from json file
+# with open('uefa_country_clubs.json') as json_source: 
+# 	source_data = json.load(json_source)
+# 	# pprint(source_data['tournaments'])
+# 	for x in source_data['tournaments']:
+# 		# pprint(x['name'])
+# 		if x['name'] == "English Premier League 2015/16":
+# 			for y in x['clubs']:
+# 				db.insert_one(y)
