b225b1beb74ff04d6f01e33a53ec07ad4047e3ac
Jerin John <jerin.j@somaiya.edu>
2017-10-23 21:18:07 +0530

    Updated ETL with FACT, OLAP and Analytical Query programs added.

---
+++ olapEngine.py
@@ -0,0 +1,42 @@
+# Program to fire queries on MongoDb Data Warehouse
+from pymongo import MongoClient
+from pprint import pprint
+
+mdb_client = MongoClient()
+db = mdb_client.uefa.playerTransfers
+
+# OLAP Queries executed on FACT
+
+# Slicing Query
+print("\nSlicing> Total Transfers per agent :- \n")
+data = db.aggregate([{"$group" : {"_id" : "$agent.id", "total_transfer" : {"$sum" : "$transfer_fee"}}}])
+
+for x in data:
+	print(x)
+
+# Dicing Query
+print("\nDicing> Total Transfers per agent per club :- \n")
+
+data = db.aggregate([{"$group" : {"_id" : {"$concat": ["$agent.id","-", "$to_club.id"]}, "total_transfer" : {"$sum" : "$transfer_fee"}}}])
+
+for x in data:
+	print(x)
+
+
+# Analytical Queries using Join operations between dimension tables.
+
+db = mdb_client.uefa
+
+print("\n> Club information for each player :-\n")
+
+data = db.playerprofiles.aggregate([{ "$lookup": { "from": "clubs", "localField": "club.id", "foreignField": "_id", "as": "club_info"}}, { "$project": { "name":1, "club_info":1 , "_id":0} }])
+
+for x in data:
+	print(x)
+
+print("\n> League information for each club :-\n")
+
+data = db.clubs.aggregate([{ "$lookup": { "from": "leagues", "localField": "league.id", "foreignField": "_id", "as": "league_info"}}, { "$project": { "name":1, "league_info":1 , "global_ranking":1, "_id":0} }])
+
+for x in data:
+	print(x)
